73cd628273fccae58f98db5e9eef83d0ab275a41
Roger Chang <roger5521610@gmail.com>
2020-07-20 11:16:28 +0800

    Initial Commit

---
+++ 暑_計概1_7.20/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  暑_計概1_7.20
+//
+//  Created by Chang Chung Che on 2020/7/20.
+//  Copyright © 2020 Chang Chung Che. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
